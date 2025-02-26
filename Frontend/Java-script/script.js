6d3ce3e6b64837d69b5a6879d4d4547dc654102c
Anish khan <aniskhanmalkan@gmail.com>
2025-02-26 18:08:49 +0530

    image upload

--- Frontend/Java-script/script.js
+++ Frontend/Java-script/script.js
@@ -1,3 +1,15 @@
+// nav
+var navLinks = document.getElementById("navLinks");
+      
+function showMenu() {
+  navLinks.style.right = "0";
+}
+
+function hideMenu() {
+  navLinks.style.right = "-200px";
+}
+
+
 // Initialize Swiper
 var swiper = new Swiper(".portfolio-slider", {
   slidesPerView: 1,
